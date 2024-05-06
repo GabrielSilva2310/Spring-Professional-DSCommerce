56da9303b3d7ba6ba244ec69671d003903825bd1
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-05-06 18:21:17 -0300

    Uploading project created

---
+++ src/main/java/com/SpringProfessional/dscommerce/enums/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.SpringProfessional.dscommerce.enums;
+
+public enum OrderStatus {
+	
+	WAITING_PAYMENT,PAID,SHIPPED,DELIVERED,CANCELED;
+
+}
---
+++ src/test/java/com/SpringProfessional/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringProfessional.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
