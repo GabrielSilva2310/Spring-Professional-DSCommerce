e279f6fc22ffa26b268d7ab1554c989b8f7d4ed0
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-06-26 01:50:01 -0300

    UserDetails, GrantedAuthority, not tested

--- src/main/java/com/SpringProfessional/dscommerce/entities/Role.java
+++ src/main/java/com/SpringProfessional/dscommerce/entities/Role.java
@@ -2,15 +2,18 @@ package com.SpringProfessional.dscommerce.entities;
 
 import java.util.Objects;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,7 +36,8 @@ public class Role {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
+	@Override
 	public String getAuthority() {
 		return authority;
 	}
