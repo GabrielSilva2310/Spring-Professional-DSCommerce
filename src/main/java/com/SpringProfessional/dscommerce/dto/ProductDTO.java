ca46d7b7be3c7af9bf0a740809bd44ad2369e82b
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-07-02 16:33:17 -0300

    Fix validation

--- src/main/java/com/SpringProfessional/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/SpringProfessional/dscommerce/dto/ProductDTO.java
@@ -8,6 +8,7 @@ import com.SpringProfessional.dscommerce.entities.Product;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -23,6 +24,7 @@ public class ProductDTO {
 	@NotBlank(message = "Campo requerido")
 	private String description;
 	
+	@NotNull(message = "Campo requerido!")
 	@Positive(message = "O preço deve ser positivo")
 	private Double price;
 	
